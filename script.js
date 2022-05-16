0fe027f849b78587c3fa4b8d24c3611534f1297b
Rubonic <dannykraemer099@gmail.com>
2022-05-16 09:58:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function play() {
+    document.getElementById("sushiVid").play;
+    document.getElementById("sushiVid").muted = true;
+}
+
+function pause() {
+    document.getElementById("sushiVid").pause;
+    document.getElementById("sushiVid").muted = true;
+}
